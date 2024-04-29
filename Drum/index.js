f677afc732adb17a143d9352e4fc016d9e45d07d
sharansgs786 <135095297+sharansgs786@users.noreply.github.com>
2024-04-28 21:00:53 -0400

    Add files via upload

---
+++ Drum/index.js
@@ -0,0 +1,58 @@
+document.addEventListener("keydown", function(event){
+        clickedButton(event.key)
+        buttonAnimation(event.key)
+})
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        clickedButton(this.innerHTML)
+        buttonAnimation(this.innerHTML)
+    })
+    
+}
+    function clickedButton(pressed){
+        switch(pressed) {
+            case "w":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+    
+            case "a":
+            var kickbass=new Audio("sounds/kick-bass.mp3");
+            kickbass.play();
+            break;
+    
+            case "s":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+    
+            case "d":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break; 
+    
+            case "j":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+    
+            case "k":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+    
+            case "l":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+    
+            default:
+        }
+}
+ function buttonAnimation(anikey){
+   document.querySelector("."+anikey).classList.add("pressed");
+   setTimeout(function(){
+document.querySelector("."+anikey).classList.remove("pressed"),600
+       })
+}
